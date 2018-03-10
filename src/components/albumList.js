c103bd0199a4db73968cfa84351724e12e5bf5d6
Hein Lin Aung <heinlinaung6891@gmail.com>
2018-03-11 02:49:41 +0630

    Added new module axios for http request

--- src/components/albumList.js
+++ src/components/albumList.js
@@ -1,7 +1,13 @@
 import React, {Component} from 'react';
 import { Text, View } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
+	componentWithMount(){
+		axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+			.then(res=>console.log('res',res))
+	}
+
   render() {
     return(
       <View>
