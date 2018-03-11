7144b6a9696c7c1f1e2757ba176a74cf4926a228
Hein Lin Aung <heinlinaung6891@gmail.com>
2018-03-11 19:04:55 +0630

    Added : thumbnail and album art images and styling

--- src/components/albumDetail.js
+++ src/components/albumDetail.js
@@ -1,16 +1,62 @@
 import React, {Component} from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card'
 import CardSection from './CardSection'
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+	const { title, artist, thumbnail_image, image } = album
+	const { 
+		thumbnailStyle, 
+		headerContentStyle, 
+		thumbnailContainerStyle,
+		headerTextStyle,
+		albumImageStyle
+	} = styles
 	return(
 		<Card>
 			<CardSection>
-				<Text>{props.album.title}</Text>
+				<View style={thumbnailContainerStyle}>
+					<Image 
+						source={{ uri : thumbnail_image }} 
+						style={thumbnailStyle} 
+					/>
+				</View>
+				<View style={headerContentStyle}>
+					<Text style={headerTextStyle}>{title}</Text>
+					<Text>{artist}</Text>
+				</View>
+			</CardSection>
+			<CardSection>
+				<View>
+					<Image source={{ uri : image }} style={albumImageStyle}/>
+				</View>
 			</CardSection>
 		</Card>		
 	)
+}
+
+const styles = {
+	headerContentStyle : {
+		justifyContent : 'space-around',
+		flexDirection : 'column'
+	},
+	headerTextStyle :{
+		fontSize : 18
+	},
+	thumbnailStyle : {
+		height : 50, 
+		width : 50
+	},
+	thumbnailContainerStyle : {
+		justifyContent : 'center',
+		alignItems : 'center',
+		marginLeft : 10,
+		marginRight : 10
+	},
+	albumImageStyle : {
+		height : 300,
+		width : 380
+	}
 }
 
 export default AlbumDetail;
